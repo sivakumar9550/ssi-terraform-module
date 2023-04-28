645f8ba4689650592627648abb7ea116175f10a0
sivakumar9550 <103304315+sivakumar9550@users.noreply.github.com>
2023-04-28 12:58:46 +0530

    Update web.tf

--- ssi-up-module-web/web.tf
+++ ssi-up-module-web/web.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_server" {
   count = var.web_want == true ? var.web_count : 0
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-02eb7a4783e7e9317"
   instance_type = var.instance_type
   key_name      = var.key_name
   
